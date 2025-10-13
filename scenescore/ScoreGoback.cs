d0304541c190bb40f8ffd76d77dabb4ec86e3d2a
mugdA001 <mugdarat.p@ku.th>
2025-10-13 09:53:53 +0700

    Add files via upload

---
+++ scenescore/ScoreGoback.cs
@@ -0,0 +1,25 @@
+using Godot;
+using System;
+
+public partial class ScoreGoback : TextureButton
+{
+	[Export] public int LevelNumber = 1;
+
+	public override void _Ready()
+	{
+		int level = GameProgress.CurrentPlayingLevel;
+		GD.Print($"Show score for Level {level}");
+		// โหลดคะแนนของเลเวลนี้มาโชว์
+	}
+
+	public override void _Pressed()
+	{
+	if (GameProgress.CurrentPlayingLevel == GameProgress.CurrentLevelIndex + 1)
+		{
+		GameProgress.Advance();
+		GD.Print("Advance to level: " + GameProgress.CurrentLevelIndex);
+		}
+
+	GetTree().ChangeSceneToFile("res://scenecheckpoint/checkpoint.tscn");
+	}
+}
